1f5bf5caba9eea56efd194cb9fbd6f035076f14c
dmitridrozdov <dm.drozdov@gmail.com>
2023-04-19 16:10:54 +1000

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,17 @@
+// import { Redirect } from "expo-router";
+
+// export default function Index() {
+//     return <Redirect href="/home" />;
+// }
+
+import { View, Text } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
